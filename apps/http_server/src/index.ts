f658e3d17cc5ffa7602208986ce696d451c1f9f9
Neeraj Reddy <neerajreddy0059@gmail.com>
2026-06-07 18:27:10 +0530

    added cors

--- apps/http_server/src/index.ts
+++ apps/http_server/src/index.ts
@@ -3,11 +3,13 @@ import "dotenv/config";
 import authRouter from "./routes/auth.js";
 import assetRouter from "./routes/assets.js";
 import candleRouter from "./routes/candles.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/auth", authRouter);
 app.use("/assets", assetRouter);
