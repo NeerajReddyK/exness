0842b7f14cc5e1af3043e4142915a53a0c97c3f5
Neeraj Reddy <neerajreddy0059@gmail.com>
2026-06-14 22:49:42 +0530

    added cookies to auth

--- apps/http_server/src/routes/auth.ts
+++ apps/http_server/src/routes/auth.ts
@@ -1,49 +1,89 @@
 import express, { Router } from "express";
 import { signUpSchema, singInSchema } from "../schema/authSchema.js";
-import { USERS } from "../store/user.js";
+import { prisma } from "../store/db.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
 const router: Router = express.Router();
 
 // create new user
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   try {
-    const { email, password, name } = signUpSchema.parse(req.body);
-
-    if (USERS[email]) {
-      return res
-        .status(400)
-        .json({ message: "user already exists with given email" });
+    const { success } = signUpSchema.safeParse(req.body);
+    if (!success) {
+      res.status(401).json({ message: "invalid input parameters" });
+      return;
     }
 
-    USERS[email] = {
-      email,
-      password,
-      name,
-      balance: { usd_balance: 5000 },
-      assets: {},
-    };
+    const { email, password, name } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 8);
 
-    return res.status(200).json({ message: "user created successfully!" });
+    const response = await prisma.user.create({
+      data: { email, password: hashedPassword, name },
+    });
+
+    const id = response.id;
+
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      return res.status(500).json({ message: "Invalid jwt token" });
+    }
+
+    const token = jwt.sign({ email, id }, jwtSecret, { expiresIn: "7d" });
+    console.log("token for auth: ", token);
+    return res
+      .cookie("auth-token", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ message: "user created successfully!" });
   } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      return res.status(401).json({ message: "email already exists" });
+    }
     return res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // log existing user in
-router.post("/signin", (req, res) => {
+router.post("/signin", async (req, res) => {
   try {
-    const { email, password } = singInSchema.parse(req.body);
-
-    if (!USERS[email]) {
-      return res.status(400).json({ message: "Invalid user" });
-    }
-    const user = USERS[email];
-    if (user.password !== password) {
-      return res.status(400).json({ message: "Invalid password" });
+    const { success } = singInSchema.safeParse(req.body);
+    if (!success) {
+      return res.status(401).json({ message: "invalid inputs" });
     }
 
-    return res.status(200).json(user);
-  } catch (error) {}
+    const { email, password } = req.body;
+    const user = await prisma.user.findFirst({ where: { email } });
+    if (!user) {
+      return res.status(404).json({ message: "user not found" });
+    }
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (!passwordCheck) {
+      return res.status(401).json({ message: "invalid password" });
+    }
+    const id = user.id;
+    const jwtSecret = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      return res.status(500).json({ message: "Invalid jwt token" });
+    }
+
+    const token = jwt.sign({ email, id }, jwtSecret, { expiresIn: "7d" });
+
+    return res
+      .cookie("auth-token", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        secure: true,
+        sameSite: true,
+      })
+      .status(200)
+      .json({ message: "success" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error", error });
+  }
 });
 
 export default router;
--- apps/http_server/src/store/db.ts
+++ apps/http_server/src/store/db.ts
@@ -1,9 +1,20 @@
 import "dotenv/config";
 import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
+// for tiemscaledb
 const dbUrl = process.env.DB_URL;
 if (!dbUrl) {
   console.log("dbUrl is undefined");
 }
 
 export const pgPool = new Pool({ connectionString: dbUrl });
+
+// for user db
+const connectionString = `${process.env.DB_URL_USER}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
